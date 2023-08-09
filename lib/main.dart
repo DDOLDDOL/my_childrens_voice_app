ca2b5add5f30a547318826d905008c6e251ba188
DDOLDDOL <doyt@naver.com>
2023-08-10 02:12:46 +0900

    refactor: Refactor main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_childrens_voice_app/api/api.dart';
+import 'package:my_childrens_voice_app/authentication/authentication.dart';
+import 'package:my_childrens_voice_app/caregiver/caregiver.dart';
+import 'package:my_childrens_voice_app/common/commons.dart';
 import 'package:my_childrens_voice_app/home/home.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
-  runApp(const _App());
+  final tokenHive = await TokenHive.build();
+  final apiClient = RestApiClient(apiUrl, tokenHive);
+
+  Bloc.observer = MyBlocObserver();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider.value(value: tokenHive),
+        Provider.value(value: apiClient),
+      ],
+      child: const _App(),
+    ),
+  );
 }
 
 class _App extends StatelessWidget {
@@ -10,12 +29,30 @@ class _App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider(
+          create: (context) => HomeRepository(context.read<RestApiClient>()),
+        ),
+        RepositoryProvider(
+          create: (context) => AuthenticationRepository(
+            context.read<RestApiClient>(),
+          ),
+        ),
+        RepositoryProvider(
+          create: (context) => CaregiverRepository(
+            context.read<RestApiClient>(),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: const Color(0xFF0D7071),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
       ),
-      home: HomeScreen(),
     );
   }
 }
