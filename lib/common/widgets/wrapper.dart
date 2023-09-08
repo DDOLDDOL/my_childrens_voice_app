b49429f82ae6f13658822c88a57f63e27443fc88
DDOLDDOL <doyt@naver.com>
2023-09-08 14:09:32 +0900

    feat: Create wrapper

--- lib/common/widgets/wrapper.dart
+++ lib/common/widgets/wrapper.dart
@@ -1,34 +1,45 @@
 import 'package:flutter/material.dart';
 
+// TODO: [BoxDecoration] 리팩토링
 class Wrapper extends StatelessWidget {
   const Wrapper({
     Key? key,
     required this.child,
     this.padding = const EdgeInsets.all(16),
-    this.backgroundColor = Colors.white,
+    this.decoration,
   }) : super(key: key);
 
   final EdgeInsets padding;
-  final Color backgroundColor;
+  final BoxDecoration? decoration;
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: padding,
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        border: Border.all(color: const Color(0xFFDDDDDD)),
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: const [
-          BoxShadow(
-            blurRadius: 4,
-            color: Color(0xFFDDDDDD),
-            offset: Offset(2, 2),
-          ),
-        ],
-      ),
+      decoration: decoration?.copyWith(
+            color: decoration?.color ?? _defaultDecoration.color!,
+            shape: decoration?.shape ?? _defaultDecoration.shape,
+            borderRadius: decoration?.shape == null
+                ? decoration?.borderRadius ?? _defaultDecoration.borderRadius!
+                : null,
+            boxShadow: decoration?.boxShadow ?? _defaultDecoration.boxShadow!,
+          ) ??
+          _defaultDecoration,
       child: child,
     );
   }
+
+  BoxDecoration get _defaultDecoration => BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 8,
+            color: Colors.black.withOpacity(0.08),
+            offset: const Offset(4, 4),
+          ),
+        ],
+      );
 }
